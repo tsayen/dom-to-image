be5619192a58b1ca40fb99151a529a4d30b47b73
tsayen <anatoly.sayenko@gmail.com>
2015-09-15 15:49:51 +0200

    minor change

--- spec/dom-to-image.spec.js
+++ spec/dom-to-image.spec.js
@@ -1,4 +1,4 @@
-(function(global) {
+(function (global) {
     'use strict';
 
     var assert = global.chai.assert;
@@ -8,67 +8,67 @@
 
     var BASE_URL = '/base/spec/resources/';
 
-    describe('domtoimage', function() {
-        this.timeout(30000);
+    describe('domtoimage', function () {
 
         afterEach(purgePage);
 
-        it('should load', function() {
+        it('should load', function () {
             assert.ok(domtoimage);
         });
 
-        it('should render simple node', function(done) {
+        it('should render simple node', function (done) {
             loadTestPage('simple/dom-node.html', 'simple/style.css', 'simple/control-image')
                 .then(renderAndCheck)
                 .then(done).catch(error);
         });
 
-        it('should render bigger node', function(done) {
+        it('should render bigger node', function (done) {
+            this.timeout(30000);
             loadTestPage('big/dom-node.html', 'big/style.css', 'big/control-image')
-                .then(function() {
-                    var parent = $('#root')[0];
-                    var child = $('.dom-child-node')[0];
+                .then(function () {
+                    var parent = $('#root');
+                    var child = $('.dom-child-node');
                     for (var i = 0; i < 1000; i++) {
-                        parent.appendChild(child.cloneNode(true));
+                        parent.append(child.clone());
                     }
                 })
                 .then(renderAndCheck)
                 .then(done).catch(error);
         });
 
-        it('should handle "#" in colors and attributes', function(done) {
+        it('should handle "#" in colors and attributes', function (done) {
             loadTestPage('hash/dom-node.html', 'hash/style.css', 'simple/control-image')
                 .then(renderAndCheck)
                 .then(done).catch(error);
         });
 
-        it('should render nested svg with broken namespace', function(done) {
+        it('should render nested svg with broken namespace', function (done) {
             loadTestPage('svg/dom-node.html', 'svg/style.css', 'svg/control-image')
                 .then(renderAndCheck)
                 .then(done).catch(error);
         });
 
-        it('should render correctly when the node is bigger than container', function(done) {
+        it('should render correctly when the node is bigger than container', function (done) {
             var domNode;
             loadTestPage('scroll/dom-node.html', 'scroll/style.css', 'scroll/control-image')
-                .then(function() {
+                .then(function () {
                     domNode = $('#root')[0];
                 })
-                .then(function() {
-                    return domtoimage.toDataUrl(domNode, function() {});
+                .then(function () {
+                    return domtoimage.toDataUrl(domNode, function () {});
                 })
                 .then(makeImage)
-                .then(function(image) {
+                .then(function (image) {
                     return drawImage(image, domNode);
                 })
                 .then(compareToControlImage)
                 .then(done).catch(error);
         });
 
-        it('should render nested text nodes', function(done) {
+        it('should render nested text nodes', function (done) {
             loadTestPage('text/dom-node.html')
-                .then(function() {
-                    domtoimage.toImage(domNode(), function(image) {
+                .then(function () {
+                    domtoimage.toImage(domNode(), function (image) {
                         drawControlImage(image);
                         assert.include(image.src, 'someText', 'text should be preserved');
                         assert.include(image.src, 'someMoreText', 'text should be preserved');
@@ -78,15 +78,15 @@
                 .catch(error);
         });
 
-        it('should render to blob', function(done) {
+        it('should render to blob', function (done) {
             loadTestPage(
                     'simple/dom-node.html',
                     'simple/style.css',
                     'simple/control-image'
                 )
-                .then(function() {
-                    checkRendering(function(domNode, callback) {
-                        domtoimage.toBlob(domNode, function(blob) {
+                .then(function () {
+                    checkRendering(function (domNode, callback) {
+                        domtoimage.toBlob(domNode, function (blob) {
                             callback(global.URL.createObjectURL(blob));
                         });
                     }, done);
@@ -94,37 +94,37 @@
                 .catch(error);
         });
 
-        describe('resource loader', function() {
+        describe('resource loader', function () {
 
-            it('should get and encode resource', function(done) {
-                getResource('fonts/fontawesome.base64').then(function(testContent) {
+            it('should get and encode resource', function (done) {
+                getResource('fonts/fontawesome.base64').then(function (testContent) {
                     domtoimage.impl.resourceLoader.load(BASE_URL + 'fonts/fontawesome.woff2')
-                        .then(function(content) {
+                        .then(function (content) {
                             assert.equal(content, testContent);
                         }).then(done).catch(error);
                 });
             });
 
-            it('should reject when resource not available', function(done) {
-                domtoimage.impl.resourceLoader.load('nonexistent_file').catch(function(error) {
+            it('should reject when resource not available', function (done) {
+                domtoimage.impl.resourceLoader.load('nonexistent_file').catch(function (error) {
                     assert.ok(error.message);
                 }).then(done);
             });
         });
 
-        describe('web fonts', function() {
+        describe('web fonts', function () {
 
             var webFontRule = domtoimage.impl.webFontRule;
 
-            it('should find all web font rules in document', function(done) {
+            it('should find all web font rules in document', function (done) {
                 loadTestPage(
                         'fonts/empty.html',
                         'fonts/rules.css'
                     )
-                    .then(function() {
+                    .then(function () {
                         return webFontRule.readAll(global.document);
                     })
-                    .then(function(fontRules) {
+                    .then(function (fontRules) {
                         var rules = fontRules.rules();
                         assert.deepEqual(Object.keys(rules), ['Font1', 'Font2']);
 
@@ -143,10 +143,10 @@
                     }).then(done).catch(error);
             });
 
-            it('should resolve relative font urls', function(done) {
-                loadTestPage('fonts/rules-relative.html').then(function() {
+            it('should resolve relative font urls', function (done) {
+                loadTestPage('fonts/rules-relative.html').then(function () {
                     return webFontRule.readAll(global.document);
-                }).then(function(fontRules) {
+                }).then(function (fontRules) {
                     var rules = fontRules.rules();
                     assert.include(Object.keys(rules['Font1'].data().urls())[0], '/base/spec/resources/font1.woff');
                     assert.include(Object.keys(rules['Font2'].data().urls())[0], '/base/spec/resources/fonts/font2.woff2');
@@ -154,25 +154,25 @@
             });
 
 
-            it('should embed web font', function(done) {
+            it('should embed web font', function (done) {
                 // given
                 var cssText, controlString;
                 Promise.all(
                         [
-                            getResource('fonts/cssText').then(function(text) {
+                            getResource('fonts/cssText').then(function (text) {
                                 cssText = text;
                             }),
-                            getResource('fonts/font-face.css').then(function(text) {
+                            getResource('fonts/font-face.css').then(function (text) {
                                 controlString = text;
                             })
                         ])
-                    .then(function() {
+                    .then(function () {
                         // when
                         var fontFaceRule = webFontRule.impl.createRule({
-                            cssText: function() {
+                            cssText: function () {
                                 return cssText;
                             },
-                            urls: function() {
+                            urls: function () {
                                 return {
                                     'http://fonts.com/font1.woff2': 'woff2',
                                     'font1.woff': 'woff'
@@ -184,7 +184,7 @@
                             'font1.woff': 'BBB'
                         }));
                     })
-                    .then(function(cssRuleString) {
+                    .then(function (cssRuleString) {
                         // then
                         assert.equal(cssRuleString, controlString);
                     })
@@ -192,22 +192,22 @@
                     .catch(error);
             });
 
-            it('should create style with web font rules', function(done) {
+            it('should create style with web font rules', function (done) {
                 // given
                 loadTestPage(
                         'fonts/empty.html',
                         'fonts/style.css'
                     )
-                    .then(function() {
+                    .then(function () {
                         return webFontRule.readAll(global.document)
-                            .then(function(webFontRules) {
+                            .then(function (webFontRules) {
                                 return webFontRules.embedAll(Object.keys(webFontRules.rules()), mockResourceLoader({
                                     'http://fonts.com/font1.woff2': 'AAA',
                                     'http://fonts.com/font2.ttf': 'CCC'
                                 }));
                             });
                     })
-                    .then(function(cssText) {
+                    .then(function (cssText) {
                         assert.include(cssText, 'url("data:font/woff2;base64,AAA")');
                         assert.include(cssText, 'url("data:font/truetype;base64,CCC")');
                     })
@@ -215,14 +215,14 @@
                     .catch(error);
             });
 
-            it.skip('should render web fonts', function(done) {
+            it.skip('should render web fonts', function (done) {
                 this.timeout(10000);
                 loadTestPage(
                         'fonts/regression.html',
                         'fonts/regression.css'
                     )
-                    .then(function() {
-                        domtoimage.toImage(domNode(), function(image) {
+                    .then(function () {
+                        domtoimage.toImage(domNode(), function (image) {
                             drawControlImage(image);
                             document.body.appendChild(image);
                             console.log(image.src);
@@ -241,18 +241,18 @@
             console.error(e.toString() + '\n' + e.stack);
         }
 
-        it('should use node filter', function(done) {
+        it('should use node filter', function (done) {
             loadTestPage(
                     'filter/dom-node.html',
                     'filter/style.css',
                     'filter/control-image'
                 )
-                .then(function() {
+                .then(function () {
                     var controlImg = $('#control-image')[0];
-                    domtoimage.toDataUrl(domNode(), function(dataUrl) {
+                    domtoimage.toDataUrl(domNode(), function (dataUrl) {
                         compare(dataUrl, controlImg, domNode(), done);
                     }, {
-                        filter: function(node) {
+                        filter: function (node) {
                             if (node.classList)
                                 return !node.classList.contains('omit');
                             return true;
@@ -263,7 +263,7 @@
 
         function compare(imgUrl, ctrlImg, node, done) {
             var img = new Image();
-            img.onload = function() {
+            img.onload = function () {
                 drawRenderedImage(img, node);
                 assert.ok(imagediff.equal(img, ctrlImg), 'rendered and control images should be equal');
                 done();
@@ -283,7 +283,7 @@
             var canvas = $('#canvas')[0];
             canvas.height = node.offsetHeight.toString();
             canvas.width = node.offsetWidth.toString();
-            makeDataUrl(node, function(dataUrl) {
+            makeDataUrl(node, function (dataUrl) {
                 checkDataUrl(dataUrl, done);
             });
         }
@@ -291,7 +291,7 @@
         function checkDataUrl(imageDataUrl, done) {
             var control = $('#control-image')[0];
             var rendered = new Image();
-            rendered.onload = function() {
+            rendered.onload = function () {
                 drawControlImage(rendered);
                 assert.ok(imagediff.equal(rendered, control), 'rendered and control images should be equal');
                 done();
@@ -307,23 +307,23 @@
         }
 
         function loadTestPage(html, css, controlImage) {
-            return loadPage().then(function() {
-                return getResource(html).then(function(html) {
-                    document.getElementById('root').innerHTML = html;
+            return loadPage().then(function () {
+                return getResource(html).then(function (html) {
+                    $('#root').html(html);
                 });
-            }).then(function() {
-                if (css) return getResource(css).then(function(css) {
-                    document.getElementById('style').appendChild(document.createTextNode(css));
+            }).then(function () {
+                if (css) return getResource(css).then(function (css) {
+                    $('#style').append(document.createTextNode(css));
                 });
-            }).then(function() {
-                if (controlImage) return getResource(controlImage).then(function(css) {
-                    document.getElementById('control-image').src = css;
+            }).then(function () {
+                if (controlImage) return getResource(controlImage).then(function (css) {
+                    $('#control-image').attr('src', css);
                 });
             })
         }
 
         function loadPage() {
-            return getResource('page.html').then(function(html) {
+            return getResource('page.html').then(function (html) {
                 var root = document.createElement('div');
                 root.id = 'test-root';
                 root.innerHTML = html;
@@ -342,8 +342,8 @@
             request.open('GET', url, true);
             request.responseType = 'text';
 
-            return new Promise(function(resolve, reject) {
-                request.onload = function() {
+            return new Promise(function (resolve, reject) {
+                request.onload = function () {
                     if (this.status == 200)
                         resolve(request.response.toString().trim());
                     else
@@ -355,9 +355,9 @@
 
 
         function makeImage(src) {
-            return new Promise(function(resolve) {
+            return new Promise(function (resolve) {
                 var image = new Image();
-                image.onload = function() {
+                image.onload = function () {
                     resolve(image);
                 };
                 image.src = src;
@@ -374,7 +374,7 @@
         }
 
         function domNodeToDataUrl() {
-            return domtoimage.toDataUrl(domNode(), function() {});
+            return domtoimage.toDataUrl(domNode(), function () {});
         }
 
         function domNode() {
@@ -403,8 +403,8 @@
 
         function mockResourceLoader(content) {
             return {
-                load: function(url) {
-                    return new Promise(function(resolve, reject) {
+                load: function (url) {
+                    return new Promise(function (resolve, reject) {
                         resolve(content[url]);
                     });
                 }
