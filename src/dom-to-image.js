2c1a15a198a171f7494e10daf50632c8d2b54611
tsayen <anatoly.sayenko@gmail.com>
2016-01-05 19:50:52 +0100

    fix unescaped delimiter in regex

--- src/dom-to-image.js
+++ src/dom-to-image.js
@@ -280,7 +280,7 @@
         }
 
         function parseExtension(url) {
-            var match = /\.([^\./]*?)$/g.exec(url);
+            var match = /\.([^\.\/]*?)$/g.exec(url);
             if (match) return match[1];
             else return '';
         }
