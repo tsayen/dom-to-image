fd2b63d30612db47020665af0877dbc218ff4cb7
soracoder <soracoder@gmail.com>
2018-11-14 12:16:54 +0100

    added tile check

--- src/dom-to-image.js
+++ src/dom-to-image.js
@@ -414,7 +414,9 @@
 
         function parseExtension(url) {
             var match = /\.([^\.\/]*?)$/g.exec(url);
+            var tile = /(\/[0-9]\/[0-9]\/[0-9])/g.exec(url)
             if (match) return match[1];
+            else if (tile) return 'jpeg';
             else return '';
         }
 
