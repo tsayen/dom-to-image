3bf3056acd4e22fc0df57b89975f9389dc000438
Marc Andrews <marcandrews@users.noreply.github.com>
2020-01-28 08:58:59 -0500

    add build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,4 +38,5 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', ['karma']);
     grunt.registerTask('default', ['jshint', 'test', 'uglify']);
+    grunt.registerTask('build', ['uglify']);
 };
