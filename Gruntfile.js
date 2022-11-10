b494e74774e2642d3b174be7b8d508f21d17ff0d
Joshua Walsh <josh@ymindustries.com>
2022-11-10 23:17:27 +1100

    Enable sourcemap generation
    This assists with debugging in applications that use this library.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,8 @@ module.exports = function(grunt) {
         },
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+                sourceMap: true
             },
             dist: {
                 files: {
