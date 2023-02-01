tsayen/dom-to-image

commit a29ba3d0cd22194ce51721ca0d57d3f0c0269c49

Bypass Trusted Type policy error (#99) (#100)

src/dom-to-image-more.js
@@ -948,7 +948,10 @@
             sandbox.style.position = 'fixed';
             document.body.appendChild(sandbox);
             // Ensure that the iframe is rendered in standard mode
-            sandbox.contentWindow.document.write('<!DOCTYPE html><meta charset="UTF-8"><title>sandbox</title><body>');
+            const charset = document.createElement('meta');
+            charset.setAttribute('charset', document.characterSet || 'UTF-8');
+            sandbox.contentDocument.head.appendChild(charset);
+            sandbox.contentDocument.title = 'sandbox';
         }
         const defaultElement = document.createElement(tagName);
         sandbox.contentWindow.document.body.appendChild(defaultElement);

commit af0d722122cf1b9a4bc275ae2e16a75a56237767

Fix the quirks mode regression This superceeds #110,  trying to fix a regression introduced by #100

src/dom-to-image-more.js
@@ -1193,20 +1193,55 @@
 
         function ensureSandboxWindow() {
             if (!sandbox) {
+                // figure out how this document is defined (doctype and charset)
+                const charsetToUse = document.characterSet || 'UTF-8';               
+                const docType = document.doctype;
+                const docTypeDeclaration = docType
+                    ? `<!DOCTYPE ${docType.name} ${docType.publicId} ${docType.systemId}`.trim() + '>'
+                    : '';
+
                 // Create a hidden sandbox <iframe> element within we can create default HTML elements and query their
                 // computed styles. Elements must be rendered in order to query their computed styles. The <iframe> won't
                 // render at all with `display: none`, so we have to use `visibility: hidden` with `position: fixed`.
                 sandbox = document.createElement('iframe');
+                sandbox.id = 'domtoimage-sandbox-' + util.uid();
                 sandbox.style.visibility = 'hidden';
                 sandbox.style.position = 'fixed';
                 document.body.appendChild(sandbox);
-                // Ensure that the iframe is rendered in standard mode
-                const charset = document.createElement('meta');
-                charset.setAttribute('charset', document.characterSet || 'UTF-8');
-                sandbox.contentDocument.head.appendChild(charset);
-                sandbox.contentDocument.title = 'sandbox';
+
+                return tryTechniques(sandbox, docTypeDeclaration, charsetToUse, 'domtoimage-sandbox');
             }
 
+            return sandbox.contentWindow;
+        }
+
+        function tryTechniques(sandbox, doctype, charset, title) {
+            // try the good old-fashioned document write with all the correct attributes set
+            try {
+                sandbox.contentWindow.document.write(`${doctype}<html><head><meta charset='${charset}'><title>${title}</title></head><body></body></html>`);
+                return sandbox.contentWindow;
+            }
+            catch (_) {
+                // swallow exception and fall through to next technique
+            }
+
+            const metaCharset = document.createElement('meta');
+            metaCharset.setAttribute('charset', charset);
+
+            // let's attempt it using srcdoc, so we can still set the doctype and charset
+            try {
+                const sandboxDocument = document.implementation.createHTMLDocument(title);
+                sandboxDocument.head.appendChild(metaCharset);
+                const sandboxHTML = doctype + sandboxDocument.documentElement.outerHTML;
+                sandbox.setAttribute('srcdoc', sandboxHTML);
+                return sandbox.contentWindow;
+            } catch (_) {
+                // swallow exception and fall through to the simplest path
+            }
+
+            // let's attempt it using srcdoc... here we're not able to set the doctype
+            sandbox.contentDocument.head.appendChild(metaCharset);
+            sandbox.contentDocument.title = title;
             return sandbox.contentWindow;
         }
 